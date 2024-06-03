97936170e93e59e6788a5a020a6f986cf4486777
pl608 <78400949+pl608@users.noreply.github.com>
2024-06-03 16:33:48 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+from zlib import decompress
+from requests import get
+
+promnts_url = os.environ('SECRET_URL')
+with open('compressed.bin','wb') as cb: cb.write(get(promnts_url).content)
+os.system('pip install gpt-2-simple pandas')
+
+with open('compressed.bin','rb') as f:
+  open('prompts.csv','wb').write(decompress(f.read()))
+
+import gpt_2_simple as gpt2
+import os
+import pandas as pd
+model_name = "124M"
+if not os.path.isdir(os.path.join("models", model_name)):
+	print(f"Downloading {model_name} model...")
+	gpt2.download_gpt2(model_name=model_name)   # model is saved into current directory under /models/124M/
+
+
+sess = gpt2.start_tf_sess()
+gpt2.finetune(sess, 'prompts.csv', model_name=model_name, steps=1000)
+
+# Reset the TensorFlow session before finetuning again
+sess.close()
+
+for root, dirs, files in os.walk('.'):
+  for file in files:
+    print(os.path.join(root, file))
