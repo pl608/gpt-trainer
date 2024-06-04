d40f455155fcbf0ba8ff3a1f764d02c07e0be8a1
pl608 <78400949+pl608@users.noreply.github.com>
2024-06-03 17:30:05 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import os
 from zlib import decompress
 
 os.system('pip install gpt-2-simple pandas requests')
-from requests import get
-promnts_url = os.environ['SECRET_URL']
-with open('compressed.bin','wb') as cb: cb.write(get(promnts_url).content)
-with open('compressed.bin','rb') as f:
-  open('prompts.csv','wb').write(decompress(f.read()))
 
+from requests import get
+
+promnts_url = os.environ['SECRET_URL']
+
+with open('compressed.bin','wb') as cb: cb.write(get(promnts_url).content)
+with open('compressed.bin','rb') as f: open('prompts.csv','wb').write(decompress(f.read()))
+print('prossesed prompts')
 import gpt_2_simple as gpt2
 import os
 import pandas as pd
@@ -16,7 +18,7 @@ if not os.path.isdir(os.path.join("models", model_name)):
 	print(f"Downloading {model_name} model...")
 	gpt2.download_gpt2(model_name=model_name)   # model is saved into current directory under /models/124M/
 
-
+print('Tuning')
 sess = gpt2.start_tf_sess()
 gpt2.finetune(sess, 'prompts.csv', model_name=model_name, steps=1000)
 
